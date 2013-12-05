d1522c2be6235b03dbd4feb98f87a2fbdf5fa96d
Emil <eculic17@gmail.com>
2013-12-05 23:19:09 +0000

    Initial commit

---
+++ src/Sample.java
@@ -0,0 +1,80 @@
+/******************************************************************************\
+* Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               *
+* Leap Motion proprietary and confidential. Not for distribution.              *
+* Use subject to the terms of the Leap Motion SDK Agreement available at       *
+* https://developer.leapmotion.com/sdk_agreement, or another agreement         *
+* between Leap Motion and you, your company or other organization.             *
+\******************************************************************************/
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import com.leapmotion.leap.*;
+
+class SampleListener extends Listener {
+    public void onInit(Controller controller) {
+        System.out.println("Initialized");
+    }
+
+    public void onConnect(Controller controller) {
+        System.out.println("Connected");
+    }
+
+    public void onDisconnect(Controller controller) {
+        //Note: not dispatched when running in a debugger.
+        System.out.println("Disconnected");
+    }
+
+    public void onExit(Controller controller) {
+        System.out.println("Exited");
+    }
+
+    public void onFrame(Controller controller) {
+        // Get the most recent frame and report some basic information
+        Frame frame = controller.frame();
+
+        if (!frame.hands().isEmpty()) {
+            // Get the first hand
+            Hand hand = frame.hands().get(0);
+
+            // Check if the hand has any fingers
+            FingerList fingers = hand.fingers();
+            if (fingers.count() > 1) {
+                // Calculate the hand's average finger tip position
+            	Vector leftTip = fingers.leftmost().tipPosition();
+                Vector rightTip = fingers.rightmost().tipPosition();
+
+                System.out.println(round(leftTip.distanceTo(rightTip), 0));
+            }
+
+        }
+    }
+    
+    public static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
+    }
+}
+
+class Sample {
+    public static void main(String[] args) {
+        // Create a sample listener and controller
+        SampleListener listener = new SampleListener();
+        Controller controller = new Controller();
+
+        // Have the sample listener receive events from the controller
+        controller.addListener(listener);
+
+        // Keep this process running until Enter is pressed
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Remove the sample listener when done
+        controller.removeListener(listener);
+    }
+}
